9e0287a98ed8f2028329acc1d910f9b6d2677ba5
Fernando <fernandoirm34@gmail.com>
2025-07-04 04:44:31 -0400

    AHORA SI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -325,7 +325,7 @@ export default function Encuesta() {
           <h2>¡Gracias por responder!</h2>
           <p>Tus respuestas han sido registradas correctamente.</p>
           <button className="boton-ir-dashboard" onClick={() => window.location.href = 'https://miradal-dash.vercel.app'}>
-            Observar información sobre el humedal y la percepción de demas usuarios sobre este.
+            Observar información y percepciones de de demás usuarios sobre el humedal
           </button>
         </div>
       )}
